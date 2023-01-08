b013d37dd308e2cefb49864c31657a5bb19bcdbc
natsurainko <a-275@qq.com>
2023-01-08 23:30:34 +0800

    Create ObservableExtension.cs

---
+++ Natsurainko.Toolkits/Values/ObservableExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Natsurainko.Toolkits.Values;
+
+public static class ObservableExtension
+{
+    public static void Sort<T, TKey>(
+        this ObservableCollection<T> collection, 
+        Func<T, TKey> keySelector,
+        IComparer<TKey> comparer,
+        bool descending = true)
+    {
+        var list = (descending
+            ? collection.OrderByDescending(keySelector, comparer)
+            : collection.OrderBy(keySelector, comparer)).ToList();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int old = collection.IndexOf(list[i]);
+
+            if (old != i)
+                collection.Move(old, i);
+        }
+    }
+
+    public static void Sort<T, TKey>(
+        this ObservableCollection<T> collection,
+        Func<T, TKey> keySelector,
+        bool descending = true)
+    {
+        var list = (descending
+            ? collection.OrderByDescending(keySelector)
+            : collection.OrderBy(keySelector)).ToList();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int old = collection.IndexOf(list[i]);
+
+            if (old != i)
+                collection.Move(old, i);
+        }
+    }
+
+    public static void Sort<T>(this ObservableCollection<T> collection, bool descending = true) where T : IComparable<T>
+    {
+        var list = (descending
+            ? collection.OrderByDescending(x => x)
+            : collection.OrderBy(x => x)).ToList();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int old = collection.IndexOf(list[i]);
+
+            if (old != i)
+                collection.Move(old, i);
+        }
+    }
+}
