4a51d508ad67ed93b63795c8b9d4ec74970ad433
natsurainko <a-275@qq.com>
2023-01-24 21:55:53 +0800

    IntExtension

---
+++ Natsurainko.Toolkits/Values/IntExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Natsurainko.Toolkits.Values;
+
+public static class IntExtension
+{
+    public static string FormatUnit(this int value)
+    {
+        double d = value;
+        int i = 0;
+
+        while ((d > 1000) && (i < 2))
+        {
+            d /= 1000;
+            i++;
+        }
+
+        var unit = new string[] { "", "K", "M"};
+        return string.Format("{0}{1}", Math.Round(d, 2), unit[i]);
+    }
+}
--- Natsurainko.Toolkits/Values/LongExtension.cs
+++ Natsurainko.Toolkits/Values/LongExtension.cs
@@ -17,7 +17,7 @@ public static class LongExtension
         }
 
         var unit = new string[] { "B", "KB", "MB", "GB", "TB" };
-        return (string.Format("{0} {1}", Math.Round(d, 2), unit[i]));
+        return string.Format("{0} {1}", Math.Round(d, 2), unit[i]);
     }
 
     public static IEnumerable<(long, long)> SplitIntoRange(this long value, int rangeCount)
